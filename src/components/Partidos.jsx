f4ac211d3ca883acf31b1e540059fc6969f725d9
eloy <eloysandoval10@hotmail.com>
2023-05-15 00:21:20 -0500

    corrigiendo2

--- src/components/Partidos.jsx
+++ src/components/Partidos.jsx
@@ -4,30 +4,79 @@ import "./styles/Partidos.css";
 const Partidos = () => {
   return (
     <article className="partidos">
-      <div className="partidos_divPadre">
-        <h4 className="partidos_h4">Primera fecha</h4>
-        <div className="partidos_div">
-          <img
-            src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgSd9wA1zTic17zYSKAvGFdwEzky3QogCWzkIX1sPnfWzmG1aw3wTLfW8F2cxmTBTgHiLFlqOb9nhfR2SKANb3jQpn_gvgiWekOolciVKPrXwrrKUgkmpVkmwuIoVF0D8KKPtD0kbYt3haNGY2jObSP-43BY1ZOnmfV66SGOrdl-SnCG-HerT5TbDWOCA/s925/primera_fecha.PNG"
-            alt=""
-          />
+
+      <div className="con">
+        <div className="aaa">
+          <h5 className="fe">Fecha 3</h5>
+          <h5 className="fe1">05/13/2023</h5>
         </div>
-      </div>
-      <div className="partidos_divPadre">
-        <h4 className="partidos_h4">Segunda fecha</h4>
-        <div className="partidos_div">
-          <img
-            src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEipCujR9yK7MNNp8TVFMtaDP-zTv_SG_bFdaw7A7rI_oKCQ0N3xNkPfFho-JeUHxpxxFAz4L9KPWGxUI794wIiR4rH38YXuoDbHLIZa5vCI9SK0wZsZ-Am01NcRN2i1LPI4otvhGiKnNuwuMsz14Tkg2zgSILf1QLVFc-IC7pmlvN59Ln0N-Ix48T37Aw/s913/segunda_fecha.PNG"
-            alt=""
-          />
+        {/* <hr className="hr" /> */}
+
+        <div className="equ">
+          
+          <div className="sss">
+            <h5 className="ganado">napoles</h5>
+            <h5 className="ganado">4</h5>
+          </div>
+          
+          <div className="sss">
+            <h5 className="perdidos">sampdoria de becerril</h5>
+            <h5>1</h5>
+          </div>
+  
         </div>
+        {/* <hr className="hr" /> */}
+
+        <div className="aaa">
+          <h5 className="fe">Fecha 2</h5>
+          <h5 className="fe1">--/--/----</h5>
+        </div>
+        {/* <hr className="hr" /> */}
+
+        <div className="equ">
+          
+          <div className="sss">
+            <h5 className="empatados">sampdoria de becerril</h5>
+            <h5>0</h5>
+          </div>
+          
+          <div className="sss">
+            <h5 className="empatados">FC internacional</h5>
+            <h5>0</h5>
+          </div>
+  
+        </div>
+        
+        {/* <hr className="hr" /> */}
+        <div className="aaa">
+          <h5 className="fe">Fecha 1</h5>
+          <h5 className="fe1">--/--/----</h5>
+        </div>
+        {/* <hr className="hr" /> */}
+
+        <div className="equ">
+          
+          <div className="sss">
+            <h5 className="perdidos">los embajadores del banco</h5>
+            <h5>0</h5>
+          </div>
+          
+          <div className="sss">
+            <h5 className="ganado">sampdoria de becerril</h5>
+            <h5 className="ganado">1</h5>
+          </div>
+  
+        </div>
+        
       </div>
-      <div>
+
+      
+      {/* <div>
         <h4 className="partidos_h4">Tabla De Posiciones</h4>
         <div className="partidos_div">
           <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgn4mavt4qlJu9ouV7MS4lmnUgT53jxvb7VGPDxzMJB_H-be3GqGCI5dnJisYasUxLGVUY23xtUPODcDA6hCGXphWQg_q4lwpbkENvtL-_B6iIMC04mbAQASNROGvRv_nH8WpyKRPK3rVW7aIZoAB4rslhqPP_5ejMdr6UMITeQDEAHa03RMekHMP5f-A/s1076/tabla.PNG" alt="" />
         </div>
-      </div>
+      </div> */}
     </article>
   );
 };
